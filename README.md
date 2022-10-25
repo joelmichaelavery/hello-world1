f2392104445197b1ec6401fb5a9221c12b05c255
joelmichaelavery <110940754+joelmichaelavery@users.noreply.github.com>
2022-10-25 16:47:46 -0500

    added hello-world.py and modfified the README.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hello-world1
  my first repository
+
+Just seeing if I can upload and pull from GitHub. 
---
+++ hello-world.py
@@ -0,0 +1,5 @@
+#Joel Avery 10/25/22
+
+#Writing a program to output Hello World just to send to GitHub
+
+print("Hello World!")
